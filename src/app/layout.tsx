1077d3a5df3ce455bfd8b9a192a6503ff70e8ae3
Ricardo Girardi <girardiricardo@outlook.com>
2025-02-14 11:26:16 -0300

    feat: add LinkedIn tracking scripts to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,6 +68,29 @@ export default function RootLayout({
           src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/48da2f9d-10ce-45ee-82e2-fd4c5a83c86a-loader.js"
           strategy="afterInteractive"
         />
+        <Script id="linkedin-partner-id" strategy="afterInteractive">
+          {`
+            _linkedin_partner_id = "8031905";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+          `}
+        </Script>
+        <Script id="linkedin-insight" strategy="afterInteractive">
+          {`
+            (function(l) {
+              if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+              window.lintrk.q=[]}
+              var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);
+            })(window.lintrk);
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{ display: "none" }} alt="" src="https://px.ads.linkedin.com/collect/?pid=8031905&fmt=gif" />
+        </noscript>
         <GoogleTagManager gtmId="G-J71H0Y6XWN" />
         <GoogleAnalytics gaId="G-J71H0Y6XWN" />
         {children}
